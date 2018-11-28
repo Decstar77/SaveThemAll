e5d8946aad9453d17aca7a4b6290e3b05a4ffc5b
decstar77 <porter.declan@gmail.com>
2018-11-28 15:10:27 +0200

    Added UI: Buttons, cursors that follow bots ect

---
+++ Assets/Scripts/CursorController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine;
+
+public class CursorController : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+
+	private bool DoWave = true;
+	[SerializeField] private float amplitude = 0.5f;
+	[SerializeField] private float period = 1f;
+	[SerializeField] private float offset = 1.5f;
+	[SerializeField] private float stepInc = 10;
+	private float step = 1;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (DoWave)
+		{
+			float stepR = step * Mathf.Deg2Rad * period;
+			float scale = amplitude * Mathf.Sin(stepR) + offset;
+			step += stepInc;
+			gameObject.transform.localScale = new Vector3(scale, scale, 0);
+		}
+		else
+		{
+			float scale = amplitude * Mathf.Sin(Mathf.PI *period * Mathf.Deg2Rad) + offset;
+			gameObject.transform.localScale = new Vector3(scale, scale, 0);
+		}
+	}
+	public void OnPointerEnter(PointerEventData eventData)
+	{
+		DoWave = false;
+	}
+	public void OnPointerExit(PointerEventData eventData)
+	{
+		DoWave = true;
+	}
+	public void Reset()
+	{
+		step = 1;
+		gameObject.transform.localScale = new Vector3(1, 1, 1);
+		gameObject.SetActive(false);
+	}
+}
---
+++ Assets/Scripts/MiniBotsManager.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniBotsManager : MonoBehaviour {
+
+	enum TimeState
+	{
+		Play = 0,
+		Pause = 1
+	}
+	enum ActionState
+	{
+		None = -1,
+		Power = 0,
+		Climb = 1
+	}
+	[SerializeField] private Canvas canvas;
+	[SerializeField] private GameObject cursor;
+	[SerializeField] private GameObject TestBot;
+
+
+	private TimeState timeState;
+	private ActionState actionState;
+	
+
+	void Start () {
+		timeState = TimeState.Play;
+		actionState = ActionState.None;
+		print(canvas.pixelRect.height);
+
+	}
+	
+	
+	void Update () {
+		switch (actionState)
+		{
+			case ActionState.None: break;
+			case ActionState.Power: DoPower(); break;
+		}
+		
+	}
+
+	private void DoPower()
+	{
+		cursor.SetActive(true);
+		Vector3 screenCords = Camera.main.WorldToScreenPoint(TestBot.transform.position);
+		cursor.transform.position = screenCords;
+	}
+
+	private void SetDefualts()
+	{
+		cursor.GetComponent<CursorController>().Reset();
+	}
+
+	/* <Summary>
+		 Contains all public functions for UI commands
+	   <Summary>*/
+	
+	public void PlayCommand()
+	{
+		switch (timeState)
+		{
+			case TimeState.Play: print("IsPlaying"); break;
+			case TimeState.Pause: print("Playing now"); timeState = TimeState.Play; break;
+		}
+	
+	}
+	public void PauseCommand()
+	{
+		switch (timeState)
+		{
+			case TimeState.Play: print("Pausing"); timeState = TimeState.Pause; ; break;
+			case TimeState.Pause: print("IsPause");  break;
+		}
+	}
+	public void PowerCommand()
+	{
+		switch(actionState)
+		{
+			case ActionState.None: actionState = ActionState.Power; break;
+			case ActionState.Power: actionState = ActionState.None; SetDefualts(); break;
+			case ActionState.Climb: actionState = ActionState.None; SetDefualts(); break;
+		}
+	}
+	public void ClimbCommand()
+	{
+
+	}
+
+}
